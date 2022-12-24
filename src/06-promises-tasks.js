4f138febfb3abdc76dd4e0b2bf6f96c5ea3d8dd1
msultanovdev <mukhammadamin.sultanov@gmail.com>
2022-12-24 14:52:59 +0500

    feat: solve problem

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -99,8 +99,18 @@ function getFastestPromise(array) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+function chainPromises(array, action) {
+  return new Promise((resolve) => {
+    const resolvedArr = [];
+    array.forEach((item) => Promise.resolve(item)
+      .then((value) => {
+        resolvedArr.push(value);
+        if (resolvedArr.length === array.length) {
+          resolve(resolvedArr.reduce(action));
+        }
+      })
+      .catch(() => resolve()));
+  });
 }
 
 module.exports = {
